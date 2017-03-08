77e7814bb0ecf37dcffb37cb9ad4626387f73817
michael james waldron <dynamikew@michaels-MacBook-Pro.local>
2017-03-08 15:59:27 -0500

    added to createState

--- features/createState/createState.route.js
+++ features/createState/createState.route.js
@@ -1,18 +1,18 @@
 angular
-    .module('createAHero.dashboard') // No array, as we are "extending" the module
-    .config(dashboardConfig);
+    .module('todoList.createdState') // No array, as we are "extending" the module
+    .config(createdStateConfig);
 
-function dashboardConfig($stateProvider) {
+function createdStateConfig($stateProvider) {
     // $stateProvider is an Angular service exposed by the ui-router library
     // this service allows us to define the "states" for our application
     //
     // REMEMBER: AngularJS is a single-page application framework. This means we
     // don't have the concept of "pages" but instead we use the concepts of "states"
     $stateProvider.state({
-        name: 'dashboard', // name of the route
-        url: '/dashboard', // url endpoint for the route
-        templateUrl: '/createAHero/features/dashboard/dashboard.html', // The url to the view template
-        controller: 'DashboardCtrl', // The name of the controller to use for this route
-        controllerAs: 'DashboardVM' // The namespace for the view to access the view model
+        name: 'dcreatedState', // name of the route
+        url: '/createdState', // url endpoint for the route
+        templateUrl: '/features/createdState/createdState.html', // The url to the view template
+        controller: 'createdStateCtrl', // The name of the controller to use for this route
+        controllerAs: 'createdStateVM' // The namespace for the view to access the view model
     })
 }
